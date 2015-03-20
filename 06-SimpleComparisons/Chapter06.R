fb2a16717cf2dde37c4f4a60668e83aab08d288f
fcampelo <fcampelo@ufmg.br>
2015-03-19 22:21:21 -0300

    Update Chapter 6 (incomplete)

--- 06-SimpleComparisons/Chapter06.R
+++ 06-SimpleComparisons/Chapter06.R
@@ -1,64 +1,123 @@
 # clean workspace
 rm(list=ls())
 
-#===================
-# Greenpeas example 01
-# Install required package
-
-if(!require(TeachingDemos)){
-	install.packages("TeachingDemos")
-	library(TeachingDemos)
+# install required packages if needed
+if (!("lmtest" %in% rownames(installed.packages()))){
+    install.packages("lmtest")
 }
-
-# load data (single vector)
-sample<-scan("../data files/greenpeas.txt")
-
-# Mean of normal distribution, variance known
-z.test(as.numeric(sample),
-       mu=500,
-       stdev=10)
-
-# Mean of normal distribution, variance unknown, one-sided test
-t.test(sample,
-       alternative = "less",
-       mu = 500,
-       conf.level = 0.99)
-
-# Mean of normal distribution, variance unknown, two-sided test
-t.test(sample,
-       mu = 500,
-       conf.level = 0.99)
-
-# Power analysis
-s<-sqrt(var(sample))
-
-# Power for the 10-observation sample
-power.t.test(n=10, 
-             delta=5, 
-             sd=s, 
-             sig.level=0.01, 
-             type = "one.sample", 
-             alternative = "one.sided")
-
-# Minimal sample size for power = 0.85
-power.t.test(power=0.85, 
-             delta=5, 
-             sd=s, 
-             sig.level=0.01, 
-             type = "one.sample", 
-             alternative = "one.sided")
+#===================
+# Calculate sample size for steel rods experiment
+(ss.calc<-power.t.test(delta=15,
+                       sd=15,
+                       sig.level=0.05,
+                       power=0.8,
+                       type="two.sample",
+                       alternative="two.sided"))
+ceiling(ss.calc$n)
 
 
-# Validation of the normality assumption
-# QQ plot
-qqnorm(sample,
-       pch=16,cex=2)
-qqline(sample,
-       lty=2,lwd=2)
+# Read steelrods data from a tab-separated file, and attach it to the workspace
+y<-read.table("../data files/steelrods.txt",
+              header=T)
+print(y)
 
-# Shapiro-Wilk test of normality
-shapiro.test(sample)
 
-# Durbin-Watson test of independence (serial autocorrelations)
+#===================
+# Example of doing the test step by step (to show some of R's syntax)
+
+n<-17   # We have 17 observations of each process
+
+# Get sample means, standard deviations
+means<-tapply(y$Length.error,
+              y$Process,
+              mean)
+
+s<-tapply(y$Length.error,
+          y$Process,
+          sd)
+
+# Calculate pooled standard deviation
+sp<-sqrt(((n-1)*s[1]^2 + (n-1)*s[2]^2)/(2*n-2))
+
+# Test statistic:
+t0<-(means[1] - means[2])/(sp*sqrt(1/n + 1/n))
+
+# Critical values
+tlims<-c(qt(.025,32),qt(.975,32))
+
+print(tlims)
+print(t0)
+
+# p-value
+p.value<-2*pt(t0,2*n-2) # multiply by two since H1 is two-sided.
+print(p.value)
+
+#===================
+# Much simpler way of doing this:
+# Perform two-sample t-test to compare means 
+# (assuming equal but unknown variances)
+with(y,
+     t.test(Length.error~Process, 
+            alternative = "two.sided", 
+            mu = 0, 
+            var.equal = TRUE, 
+            conf.level = 0.95))
+
+#===================
+# Verify the assumptions: 
+#   normality, equality of variances, independence of residuals.
+
+# Vector of residuals
+# (invert the order of the means vector to get it in the right order
+resid<-y$Length.error - rep(means[2:1],
+                            each=n)
+
+# Normality: Shapiro-Wilk test
+shapiro.test(resid)
+
+pdf("../figs/steelrodsqq.pdf",
+    width=5,
+    height=5) # comment to open plot in R
+
+library(car)
+qqPlot(resid,
+       pch=16,
+       cex=1.5,
+       las=1)
+
+dev.off() # comment this if you commented the pdf command.
+
+
+# Equality of variances: Fligner-Killeen test
+with(y,
+     fligner.test(Length.error~Process))
+
+pdf("../figs/steelrodsvar.pdf",
+    width=5,
+    height=5) # comment to open plot in R
+
+plot(x=rep(means[2:1],
+           each=n),
+     y=resid,
+     xlab="mean",
+     pch=16,
+     cex=1.5,
+     las=1)
+
+dev.off() # comment this if you commented the pdf command.
+
 library(lmtest)
-dwtest(sample~1)
+with(y,
+     dwtest(Length.error~Process))
+
+pdf("../figs/steelrodsind.pdf",
+    width=5,
+    height=5) # comment to open plot in R
+
+plot(resid,
+     pch=16,
+     cex=1.5,
+     type="b",
+     las=1)
+
+dev.off() # comment this if you commented the pdf command.
