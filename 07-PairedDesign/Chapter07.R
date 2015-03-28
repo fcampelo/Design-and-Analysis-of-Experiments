e6f9d5b31bf150378c1d19af367808a7dd0833e4
fcampelo <fcampelo@ufmg.br>
2015-03-28 16:53:07 -0300

    Update Ch.7 (still incomplete)

--- 07-PairedDesign/Chapter07.R
+++ 07-PairedDesign/Chapter07.R
@@ -6,3 +6,9 @@ if (!("lmtest" %in% rownames(installed.packages()))){
     install.packages("lmtest")
 }
 #===================
+
+# Paired design (across-problems comparison of algorithms)
+
+delta_min<-5
+# aggregate(Time~Problem:Algorithm,data=x,FUN=mean)
+# with(sumt,t.test(Time[1:7],Time[8:14],paired=T))
